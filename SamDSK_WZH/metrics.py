a0925bd2f1c9a858205e4c46d8ca7d632d3e841b
wzh <992677512@qq.com>
2023-10-25 09:22:16 +0800

    【SamDSK】增加SamDSK模型，训练，测试代码

---
+++ SamDSK_WZH/metrics.py
@@ -0,0 +1,304 @@
+import torch
+from torch.nn.functional import cross_entropy
+from torch.nn.modules.loss import _WeightedLoss
+import numpy as np
+from scipy.spatial import cKDTree
+
+def iou_metrics(array1, array2):
+    # 计算交集
+    intersection = np.logical_and(array1, array2)
+    # 计算并集
+    union = np.logical_or(array1, array2)
+    # 计算交并比
+    intersection_sum = np.sum(intersection)
+    union_sum = np.sum(union)
+
+    # 添加分母项来防止出现NaN
+    if union_sum == 0:
+        iou_score = 0.0
+    else:
+        iou_score = intersection_sum / union_sum
+
+    return iou_score
+
+EPSILON = 1e-32
+
+
+class LogNLLLoss(_WeightedLoss):
+    __constants__ = ['weight', 'reduction', 'ignore_index']
+
+    def __init__(self, weight=None, size_average=None, reduce=None, reduction=None,
+                 ignore_index=-100):
+        super(LogNLLLoss, self).__init__(weight, size_average, reduce, reduction)
+        self.ignore_index = ignore_index
+
+    def forward(self, y_input, y_target):
+        # y_input = torch.log(y_input + EPSILON)
+        return cross_entropy(y_input, y_target, weight=self.weight,
+                             ignore_index=self.ignore_index)
+
+
+def classwise_iou(output, gt):
+    """
+    Args:
+        output: torch.Tensor of shape (n_batch, n_classes, image.shape)
+        gt: torch.LongTensor of shape (n_batch, image.shape)
+    """
+    dims = (0, *range(2, len(output.shape)))
+    gt = torch.zeros_like(output).scatter_(1, gt[:, None, :], 1)
+    intersection = output*gt
+    union = output + gt - intersection
+    classwise_iou = (intersection.sum(dim=dims).float() + EPSILON) / (union.sum(dim=dims) + EPSILON)
+
+    return classwise_iou
+
+
+def classwise_f1(output, gt):
+    """
+    Args:
+        output: torch.Tensor of shape (n_batch, n_classes, image.shape)
+        gt: torch.LongTensor of shape (n_batch, image.shape)
+    """
+
+    epsilon = 1e-20
+    n_classes = output.shape[1]
+
+    output = torch.argmax(output, dim=1)
+    true_positives = torch.tensor([((output == i) * (gt == i)).sum() for i in range(n_classes)]).float()
+    selected = torch.tensor([(output == i).sum() for i in range(n_classes)]).float()
+    relevant = torch.tensor([(gt == i).sum() for i in range(n_classes)]).float()
+
+    precision = (true_positives + epsilon) / (selected + epsilon)
+    recall = (true_positives + epsilon) / (relevant + epsilon)
+    classwise_f1 = 2 * (precision * recall) / (precision + recall)
+
+    return classwise_f1
+
+
+def make_weighted_metric(classwise_metric):
+    """
+    Args:
+        classwise_metric: classwise metric like classwise_IOU or classwise_F1
+    """
+
+    def weighted_metric(output, gt, weights=None):
+
+        # dimensions to sum over
+        dims = (0, *range(2, len(output.shape)))
+
+        # default weights
+        if weights == None:
+            weights = torch.ones(output.shape[1]) / output.shape[1]
+        else:
+            # creating tensor if needed
+            if len(weights) != output.shape[1]:
+                raise ValueError("The number of weights must match with the number of classes")
+            if not isinstance(weights, torch.Tensor):
+                weights = torch.tensor(weights)
+            # normalizing weights
+            weights /= torch.sum(weights)
+
+        classwise_scores = classwise_metric(output, gt).cpu()
+
+        return classwise_scores 
+
+    return weighted_metric
+
+def ObjectF1score(S, G):
+    S = np.single(S)
+    G = np.single(G)
+
+    listS = np.unique(S)
+    listS = listS[listS != 0]
+    numS = len(listS)
+
+    listG = np.unique(G)
+    listG = listG[listG != 0]
+    numG = len(listG)
+
+    if numS == 0 and numG == 0:
+        score = 1
+        return score
+    elif numS == 0 or numG == 0:
+        score = 0
+        return score
+
+    tempMat = np.zeros((numS, 3), dtype=int)
+    tempMat[:, 0] = listS
+
+    for iSegmentedObj in range(numS):
+        intersectGTObjs = G[S == tempMat[iSegmentedObj, 0]]
+        intersectGTObjs = intersectGTObjs[intersectGTObjs != 0]
+        if len(intersectGTObjs) > 0:
+            listOfIntersectGTObjs, N = np.unique(intersectGTObjs, return_counts=True)
+            maxId = np.argmax(N)
+            tempMat[iSegmentedObj, 1] = listOfIntersectGTObjs[maxId]
+
+    for iSegmentedObj in range(numS):
+        if tempMat[iSegmentedObj, 1] != 0:
+            SegObj = S == tempMat[iSegmentedObj, 0]
+            GTObj = G == tempMat[iSegmentedObj, 1]
+            overlap = np.logical_and(SegObj, GTObj)
+            areaOverlap = np.sum(overlap)
+            areaGTObj = np.sum(GTObj)
+            if areaOverlap / areaGTObj > 0.5:
+                tempMat[iSegmentedObj, 2] = 1
+
+    TP = np.sum(tempMat[:, 2] == 1)
+    FP = np.sum(tempMat[:, 2] == 0)
+    FN = numG - TP
+
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+
+    score = (2 * precision * recall) / (precision + recall)
+    return score, TP, FP, FN
+
+
+def ObjectHausdorff(S, G):
+    S = S.astype(np.float32)
+    G = G.astype(np.float32)
+
+    totalAreaS = np.sum(S > 0)
+    totalAreaG = np.sum(G > 0)
+
+    listLabelS = np.unique(S)
+    listLabelS = listLabelS[listLabelS != 0]
+
+    listLabelG = np.unique(G)
+    listLabelG = listLabelG[listLabelG != 0]
+
+    temp1 = 0
+    for iLabelS in listLabelS:
+        Si = S == iLabelS
+        intersectlist = G[Si]
+        intersectlist = intersectlist[intersectlist != 0]
+
+        if intersectlist.size > 0:
+            indexGi = np.bincount(intersectlist.astype(int)).argmax()
+            Gi = G == indexGi
+        else:
+            tempDist = np.zeros(len(listLabelG))
+            for iLabelG in range(len(listLabelG)):
+                Gi = G == listLabelG[iLabelG]
+                tempDist[iLabelG] = Hausdorff(Gi, Si)
+            minIdx = np.argmin(tempDist)
+            Gi = G == listLabelG[minIdx]
+
+        omegai = np.sum(Si) / totalAreaS
+        temp1 += omegai * Hausdorff(Gi, Si)
+
+    temp2 = 0
+    for iLabelG in listLabelG:
+        tildeGi = G == iLabelG
+        intersectlist = S[tildeGi]
+        intersectlist = intersectlist[intersectlist != 0]
+
+        if intersectlist.size > 0:
+            indextildeSi = np.bincount(intersectlist.astype(int)).argmax()
+            tildeSi = S == indextildeSi
+        else:
+            tempDist = np.zeros(len(listLabelS))
+            for iLabelS in range(len(listLabelS)):
+                tildeSi = S == listLabelS[iLabelS]
+                tempDist[iLabelS] = Hausdorff(tildeGi, tildeSi)
+            minIdx = np.argmin(tempDist)
+            tildeSi = S == listLabelS[minIdx]
+
+        tildeOmegai = np.sum(tildeGi) / totalAreaG
+        temp2 += tildeOmegai * Hausdorff(tildeGi, tildeSi)
+
+    objHausdorff = (temp1 + temp2) / 2
+    return objHausdorff
+
+
+def Hausdorff(S, G):
+    S = S.astype(np.float32)
+    G = G.astype(np.float32)
+
+    maskS = S > 0
+    maskG = G > 0
+    rowInd, colInd = np.indices(S.shape)
+    coordinates = np.column_stack((rowInd[maskG], colInd[maskG]))
+
+    x = coordinates
+    y = np.column_stack((rowInd[maskS], colInd[maskS]))
+
+    kdtree = cKDTree(y)
+    dist, _ = kdtree.query(x)
+    dist1 = np.max(dist)
+
+    kdtree = cKDTree(x)
+    dist, _ = kdtree.query(y)
+    dist2 = np.max(dist)
+
+    hausdorffDistance = max(dist1, dist2)
+    return hausdorffDistance
+
+def Dice(A, B):
+    temp = np.logical_and(A, B)
+    dice = 2 * np.sum(temp) / (np.sum(A) + np.sum(B))
+    return dice
+
+def ObjectDice(S, G):
+    S = np.single(S)
+    G = np.single(G)
+
+    listLabelS = np.unique(S)
+    listLabelS = listLabelS[listLabelS != 0]
+    numS = len(listLabelS)
+
+    listLabelG = np.unique(G)
+    listLabelG = listLabelG[listLabelG != 0]
+    numG = len(listLabelG)
+
+    if numS == 0 and numG == 0:
+        objDice = 1
+        return objDice
+    elif numS == 0 or numG == 0:
+        objDice = 0
+        return objDice
+
+    totalAreaS = np.sum(S > 0)
+    temp1 = 0
+    for iLabelS in listLabelS:
+        Si = (S == iLabelS)
+        intersectlist = G[Si]
+        intersectlist = intersectlist[intersectlist != 0]
+
+        if len(intersectlist) != 0:
+            indexGi = np.argmax(np.bincount(intersectlist.astype(int)))
+            Gi = (G == indexGi)
+        else:
+            Gi = np.zeros_like(G, dtype=bool)
+
+        omegai = np.sum(Si) / totalAreaS
+        temp1 += omegai * Dice(Gi, Si)
+
+    totalAreaG = np.sum(G > 0)
+    temp2 = 0
+    for iLabelG in listLabelG:
+        tildeGi = (G == iLabelG)
+        intersectlist = S[tildeGi]
+        intersectlist = intersectlist[intersectlist != 0]
+
+        if len(intersectlist) != 0:
+            indextildeSi = np.argmax(np.bincount(intersectlist.astype(int)))
+            tildeSi = (S == indextildeSi)
+        else:
+            tildeSi = np.zeros_like(S, dtype=bool)
+
+        tildeOmegai = np.sum(tildeGi) / totalAreaG
+        temp2 += tildeOmegai * Dice(tildeGi, tildeSi)
+
+    objDice = (temp1 + temp2) / 2
+    return objDice
+
+
+jaccard_index = make_weighted_metric(classwise_iou)
+f1_score = make_weighted_metric(classwise_f1)
+
+
+if __name__ == '__main__':
+    output, gt = torch.zeros(3, 2, 5, 5), torch.zeros(3, 5, 5).long()
+    print(classwise_iou(output, gt))
---
+++ SamDSK_WZH/train.py
@@ -0,0 +1,318 @@
+# Code for MedT
+
+import argparse
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import os
+from dataloader import DataFolder
+import numpy as np
+import torch
+from my_transforms import get_transforms
+from loss import dice_loss, object_dice_losses
+import utils
+from utils import accuracy_pixel_level
+from models.swin_unet import SwinUnet
+import logging
+
+parser = argparse.ArgumentParser(description='MedT')
+parser.add_argument('--num_workers', default=4, type=int, help='number of data loading workers (default: 8)')
+parser.add_argument('--epochs', default=500, type=int, help='number of total epochs to run(default: 400)')
+parser.add_argument('--start-epoch', default=0, type=int, help='manual epoch number (useful on restarts)')
+parser.add_argument('--batch_size', default=4, type=int, help='batch size (default: 1)')
+## GlaS 1e-4，这个非常重要，学习率轻微调整都对结果有很大影响, CRAG 5e-5；
+parser.add_argument('--learning_rate', default=5e-5, type=float, help='initial learning rate (default: 0.001)')
+parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=1e-5, type=float, help='weight decay (default: 1e-5)')
+parser.add_argument('--save_freq', type=int,default = 50)
+parser.add_argument('--dataset', type=str, choices=['GlaS', 'CRAG'], default='CRAG', help='which dataset be used')
+
+parser.add_argument('--modelname', default='swinUnet', type=str, help='type of model')
+parser.add_argument('--cuda', default="on", type=str, help='switch on/off cuda option (default: on)')
+parser.add_argument('--aug', default='off', type=str, help='turn on img augmentation (default: False)')
+parser.add_argument('--load', default='default', type=str, help='load a pretrained model')
+parser.add_argument('--save_dir', type=str, default='./experiments/')
+parser.add_argument('--crop', type=int, default=448)
+parser.add_argument('--imgsize', type=int, default=448)
+parser.add_argument('--gray', default='no', type=str)
+parser.add_argument('--device', default='cuda:3', type=str)
+parser.add_argument('--gpu', type=list, default=[3], help='GPUs for training')
+
+parser.add_argument('--gamma1', type=float, default=1, help='weight for dice loss')
+parser.add_argument('--gamma2', type=float, default=0.5, help='weight for object-level dice loss')
+args = parser.parse_args()
+
+gray_ = args.gray
+aug = args.aug
+modelname = args.modelname
+imgsize = args.imgsize
+torch.cuda.set_device(args.device)
+
+def main():
+    global logger
+
+    if args.crop is not None:
+        crop = (args.crop, args.crop)
+    else:
+        crop = None
+
+    # set up logger
+    logger, logger_results = setup_logging()
+
+
+    # ---------------- load data -------------------- #
+    # if args.dataset == 'GlaS':
+    #     img_dir = '/home/data2/MedImg/GlandSeg/%s/wzh/train/480x480/' % (args.dataset)
+    # elif args.dataset == 'CRAG':
+    #     img_dir = '/home/data2/MedImg/GlandSeg/%s/wzh/train/480x480/' % (args.dataset)
+    #
+    # tf_train = JointTransform2D(crop=crop, p_flip=0.5, color_jitter_params=(0.1, 0.1, 0.1, 0.1), long_mask=True)
+    # tf_val = JointTransform2D(crop=crop, p_flip=0, color_jitter_params=None, long_mask=True)
+    # train_dataset = ImageToImage2D(img_dir, tf_train)
+    # val_dataset = ImageToImage2D(img_dir, tf_val)
+    #
+    # train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    # val_loader = DataLoader(val_dataset, 1, shuffle=True)
+    # ---------------------------------------------- #
+    # ----- define augmentation ----- #
+    data_transforms = {
+        'train': get_transforms({
+        'random_crop' : args.imgsize,
+        'horizontal_flip': True,
+        'vertical_flip': True,
+        'random_rotation': 90,
+        'to_tensor': 1,
+    }),
+        'val': get_transforms({
+        'random_crop': args.imgsize,
+        'to_tensor': 1,
+    })}
+
+    dsets = {}
+    for x in ['train', 'val']:
+        # img_dir = '/home/data2/MedImg/GlandSeg/%s/wzh/train/480x480/Images/' % (args.dataset)
+        # target_dir = '/home/data2/MedImg/GlandSeg/%s/wzh/train/480x480/Annotation/' % (args.dataset)
+        ## GlaS
+        img_dir = '/home/data2/MedImg/GlandSeg/%s/train/Images' % (args.dataset)
+        target_dir = '/home/data2/MedImg/GlandSeg/%s/train/Annotation' % (args.dataset)
+        ## CRAG
+        # img_dir = '/home/data2/MedImg/GlandSeg/%s/train/TrainSet/Images' % (args.dataset)
+        # target_dir = '/home/data2/MedImg/GlandSeg/%s/train/TrainSet/Annotation' % (args.dataset)
+        dir_list = [img_dir, target_dir]
+        # post_fix = ['weight.png', 'label.png']
+        if x == 'train':
+            dsets[x] = DataFolder(dir_list, data_transform=data_transforms[x], semi_rate=0.1)
+        else:
+            dsets[x] = DataFolder(dir_list, data_transform=data_transforms[x])
+    train_loader = DataLoader(dsets['train'], batch_size=args.batch_size, shuffle=True,
+                              num_workers=args.num_workers)
+    val_loader = DataLoader(dsets['val'], batch_size=1, shuffle=False,
+                            num_workers=args.num_workers)
+    ########################################################################
+
+
+    # ----- create model ----- #
+    device = torch.device(args.device)
+    model = SwinUnet(img_size = imgsize, num_classes = 2)
+    logger.info("=> Creating {} model!".format(modelname))
+
+    # ------ 加载预训练权重 ------ #
+    # model.load_state_dict(torch.load("swin_tiny_patch4_window7_224.pth"))
+    model.load_from(args.device, logger, "../MedT/swin_tiny_patch4_window7_224.pth")
+    logger.info("=> Loaded pretrained model weight!")
+
+    if len(args.gpu) > 1:
+        print("Let's use", len(args.gpu), "GPUs!")
+        model = nn.DataParallel(model, device_ids=args.gpu)
+    model.to(device)
+
+    # criterion = LogNLLLoss()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(list(model.parameters()), lr=args.learning_rate, betas=(0.9, 0.99), weight_decay=1e-4)
+    ## GlaS 学习率调整策略
+    # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[200, 400], gamma=0.1)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[200, 400], gamma=0.5)
+
+    pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    logger.info("=> Total_params: {}".format(pytorch_total_params))
+
+    seed = 3000
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # torch.set_deterministic(True)
+    # random.seed(seed)
+
+    for epoch in range(args.epochs):
+        train(train_loader, model, optimizer, criterion, epoch)
+        # if epoch == 10:
+        #     for param in model.parameters():
+        #         param.requires_grad =True
+        if ((epoch + 1) % args.save_freq) ==0:
+            with torch.no_grad():
+                validate(val_loader, model, criterion)
+
+            save_dir = "%s/%s_10labeled/%d/" % (args.save_dir, args.dataset, epoch + 1)
+            if not os.path.exists(save_dir):
+                os.mkdir(save_dir)
+            torch.save(model.state_dict(), save_dir + args.modelname + ".pth")
+        scheduler.step()
+
+
+def setup_logging():
+    mode = 'w'
+
+    save_dir = "%s/%s_10labeled/" % (args.save_dir, args.dataset)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    # create logger for training information
+    logger = logging.getLogger('train_logger')
+    logger.setLevel(logging.DEBUG)
+    # create console handler and file handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    file_handler = logging.FileHandler('{:s}/train.log'.format(save_dir, mode=mode))
+    file_handler.setLevel(logging.DEBUG)
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s\t%(message)s', datefmt='%Y-%m-%d %I:%M')
+    # add formatter to handlers
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    # add handlers to logger
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    # create logger for epoch results
+    logger_results = logging.getLogger('results')
+    logger_results.setLevel(logging.DEBUG)
+    file_handler2 = logging.FileHandler('{:s}/epoch_results.txt'.format(save_dir), mode=mode)
+    file_handler2.setFormatter(logging.Formatter('%(message)s'))
+    logger_results.addHandler(file_handler2)
+
+    logger.info('***** Training starts *****')
+    logger.info('save directory: {:s}'.format(save_dir))
+    if mode == 'w':
+        logger_results.info('epoch\ttrain_loss\ttrain_loss_CE\ttrain_loss_var\ttrain_acc\ttrain_iou\t'
+                            'val_loss\tval_acc\tval_iou')
+
+    return logger, logger_results
+
+def train(train_loader, model, optimizer, criterion, epoch):
+    epoch_running_loss = 0
+    results = utils.AverageMeter(6)
+    for batch_idx, (imgs, labels, *rest) in enumerate(train_loader):
+        imgs = Variable(imgs.to(device=args.device))
+        labels = Variable(labels.to(device=args.device))
+        seg_labels = torch.gt(labels, 0).int().type(torch.LongTensor)
+        seg_labels = seg_labels.to(device=args.device)
+
+        # ===================forward=====================
+        output = model(imgs)
+
+        # measure accuracy and record loss
+        pred = np.argmax(output.data.cpu().numpy(), axis=1)
+        pred = pred.astype(int)
+        metrics = accuracy_pixel_level(pred, seg_labels.detach().cpu().numpy())
+        pixel_accu, iou = metrics[0], metrics[1]
+
+        # compute dice loss
+        score = F.softmax(output, dim=1)
+        loss_dice = dice_loss(seg_labels, score[:, 1, :, :])
+        # compute object-level dice loss
+        loss_obj_dice = object_dice_losses(labels, score[:, 1, :, :])
+        # compute ce loss
+        loss_ce = criterion(output, seg_labels)
+        loss = loss_ce + args.gamma1 * loss_dice + args.gamma2 * loss_obj_dice
+
+        result = [loss.item(),loss_ce.item(), loss_dice.item(), loss_obj_dice.item(), pixel_accu, iou]
+        results.update(result, 1)
+
+        yHaT = pred
+        yval = seg_labels
+        # ===================backward====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % 100 == 0:
+            logger.info('\tIteration: [{:d}/{:d}]'
+                        '\tLoss: {r[0]:.4f}'
+                        '\tLoss_CE {r[1]:.4f}'
+                        '\tLoss_Dice {r[2]:.4f}'
+                        '\tLoss_Obj_Dice {r[3]:.4f}'
+                        '\tPixel_Accu {r[4]:.4f}'
+                        '\tIoU {r[5]:.4f}'.format(batch_idx, len(train_loader), r=results.avg))
+
+    # ===================log========================
+    logger.info('\t=> Train Epoch [{:d}/{:d}]'
+                '\tAvg Loss: {r[0]:.4f}'
+                '\tLoss_CE {r[1]:.4f}'
+                '\tLoss_Dice {r[2]:.4f}'
+                '\tLoss_Obj_Dice {r[3]:.4f}'
+                '\tPixel_Accu {r[4]:.4f}'
+                '\tIoU {r[5]:.4f}'.format(epoch, args.epochs, r=results.avg))
+
+def validate(val_loader, model, criterion):
+    results = utils.AverageMeter(6)
+
+    for batch_idx, (imgs, labels, *rest) in enumerate(val_loader):
+        # if isinstance(rest[0][0], str):
+        #     image_filename = rest[0][0]
+        # else:
+        #     image_filename = '%s.png' % str(batch_idx + 1).zfill(3)
+
+        imgs = Variable(imgs.to(device=args.device))
+        labels = Variable(labels.to(device=args.device))
+        seg_labels = torch.gt(labels, 0).int().type(torch.LongTensor)
+        seg_labels = seg_labels.to(device=args.device)
+        # start = timeit.default_timer()
+        output = model(imgs)
+        # stop = timeit.default_timer()
+        # print('Time: ', stop - start)
+
+        # measure accuracy and record loss
+        pred = np.argmax(output.data.cpu().numpy(), axis=1)
+        pred = pred.astype(int)
+        metrics = accuracy_pixel_level(pred, seg_labels.detach().cpu().numpy())
+        pixel_accu, iou = metrics[0], metrics[1]
+
+        # compute dice loss
+        score = F.softmax(output, dim=1)
+        loss_dice = dice_loss(seg_labels, score[:, 1, :, :])
+        # compute object-level dice loss
+        loss_obj_dice = object_dice_losses(labels, score[:, 1, :, :])
+        # compute ce loss
+        loss_ce = criterion(output, seg_labels)
+        loss = loss_ce + args.gamma1 * loss_dice + args.gamma2 * loss_obj_dice
+
+        result = [loss.item(), loss_ce.item(), loss_dice.item(), loss_obj_dice.item(), pixel_accu, iou]
+        results.update(result, 1)
+
+        # print(np.unique(tmp2))
+        yHaT = pred
+        yval = seg_labels
+
+        epsilon = 1e-20
+
+        del imgs, labels, pred, seg_labels, output
+
+        yHaT[yHaT == 1] = 255
+        yval[yval == 1] = 255
+        # cv2.imwrite(fulldir+image_filename, yHaT[0,1,:,:])
+    # ===================log========================
+    logger.info('\t=> Val Avg: Loss: {r[0]:.4f}'
+                '\tLoss_CE {r[1]:.4f}'
+                '\tLoss_Dice {r[2]:.4f}'
+                '\tLoss_Obj_Dice {r[3]:.4f}'
+                '\tPixel_Accu {r[4]:.4f}'
+                '\tIoU {r[5]:.4f}'.format(r=results.avg))
+
+
+if __name__ == '__main__':
+    main()
+
+            
+
+
